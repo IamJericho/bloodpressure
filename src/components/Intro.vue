b13bb5fecd551e6b4d6ca5e4f4a59dcd302de8a5
IamJericho <HerringClinton@gmail.com>
2019-05-28 12:26:33 -0500

    adding calculator structure

---
+++ src/components/Intro.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="calculator-body">
+    <h1>{{ msg }}</h1>
+    <div class="row">
+      <div class="">
+        <label for="systolic"><strong>Systolic</strong><br>mm Hg (upper #)</label>
+        <input id="systolic" type="number" class="form-control required">
+      </div>
+      <div class="">
+        <label for="diastolic"><strong>Diastolic</strong><br>mm Hg (lower #)</label>
+        <input id="diastolic" type="number" class="form-control">
+      </div>
+      <div id="go-btn" class="">
+        <button id="submit" class="btn btn-danger btn-lg">Go</button>
+      </div>
+    </div>
+
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Intro",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
