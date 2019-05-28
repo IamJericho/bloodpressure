6d82ea05bfd08286e9653866d7e827f1ba21063b
IamJericho <HerringClinton@gmail.com>
2019-05-28 14:19:48 -0500

    added font awesome icons

--- src/components/HistoryResults.vue
+++ src/components/HistoryResults.vue
@@ -3,7 +3,7 @@
     <h1>{{ msg }}</h1>
     <div class="row">
       <div class="col col-sm-6 offset-sm-3">
-        <ul>
+        <ul class="list-unstyled">
           <li>results</li>
         </ul>
       </div>
