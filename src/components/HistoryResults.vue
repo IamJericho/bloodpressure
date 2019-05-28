20c21101ae0582045f03cb1ecaf4d20e77ae950e
IamJericho <HerringClinton@gmail.com>
2019-05-28 14:14:50 -0500

    added history page

---
+++ src/components/HistoryResults.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="calculator-body">
+    <h1>{{ msg }}</h1>
+    <div class="row">
+      <div class="col col-sm-6 offset-sm-3">
+        <ul>
+          <li>results</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "History",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/views/History.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="history-body">
+    <History msg="Blood Pressure History" />
+  </div>
+</template>
+
+<script>
+  // @ is an alias to /src
+  import History from "@/components/HistoryResults.vue";
+
+  export default {
+    name: "history",
+    components: {
+      History
+    }
+  };
+</script>
