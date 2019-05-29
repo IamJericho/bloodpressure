9b7a9ee5ddd0696a40cafce646f4277ab5a2053c
IamJericho <HerringClinton@gmail.com>
2019-05-29 12:58:52 -0500

    combined results and initial view components

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -1,30 +1,51 @@
 <template>
   <div class="calculator-body">
     <h1>{{ msg }}</h1>
-    <div class="row">
-      <div class="col col-sm-6 offset-sm-3">
-        <label for="systolic">
-          <strong>Systolic</strong>
-          <br />mm Hg (upper #)
-        </label>
-        <input id="systolic" type="number" class="form-control required" />
+    <form>
+      <div class="row">
+        <div class="col col-sm-6 offset-sm-3">
+          <label for="systolic">
+            <strong>Systolic</strong>
+            <br />mm Hg (upper #)
+          </label>
+          <input id="systolic" type="number" class="form-control required" />
+        </div>
       </div>
-    </div>
-    <div class="row mt-4">
-      <div class="col col-sm-6 offset-sm-3">
-        <label for="diastolic">
-          <strong>Diastolic</strong>
-          <br />mm Hg (lower #)
-        </label>
-        <input id="diastolic" type="number" class="form-control" />
+      <div class="row mt-4">
+        <div class="col col-sm-6 offset-sm-3">
+          <label for="diastolic">
+            <strong>Diastolic</strong>
+            <br />mm Hg (lower #)
+          </label>
+          <input id="diastolic" type="number" class="form-control" />
+        </div>
       </div>
-    </div>
+      <div class="row">
+        <div id="go-btn" class="col">
+          <button
+            id="submit"
+            type="submit"
+            class="btn btn-success btn-lg mt-4 mr-4"
+          >
+            Submit
+          </button>
+          <button
+            id="reset"
+            type="reset"
+            class="btn btn-danger btn-lg mt-4 ml-4"
+          >
+            Reset
+          </button>
+        </div>
+      </div>
+    </form>
     <div class="row">
-      <div id="go-btn" class="col">
-        <button id="submit" class="btn btn-success btn-lg mt-4 mr-4">Go</button>
-        <button id="reset" class="btn btn-danger btn-lg mt-4 ml-4">
-          Reset
-        </button>
+      <div class="results-body mt-5 col">
+        <div id="results" class="bg-color">
+          <div>
+            <p class="text-center">Results</p>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -35,9 +56,59 @@ export default {
   name: "Intro",
   props: {
     msg: String
-  }
+  },
+  categories: [
+    {
+      categoryId: 1,
+      categoryMsg: "Normal",
+      categoryColor: "green"
+    },
+    {
+      categoryId: 2,
+      categoryMsg: "Elevated",
+      categoryColor: "yellow"
+    },
+    {
+      categoryId: 3,
+      categoryMsg: "High Blood Pressure",
+      categoryWarning: "Hypertension Stage 1",
+      categoryColor: "orange"
+    },
+    {
+      categoryId: 4,
+      categoryMsg: "High Blood Pressure",
+      categoryWarning: "Hypertension Stage 2",
+      categoryColor: "red-orange"
+    },
+    {
+      categoryId: 5,
+      categoryMsg: "Hypertensive Crisis",
+      categoryWarning: "Consult your doctor immediately",
+      categoryColor: "red"
+    }
+  ]
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped></style>
+<style scoped>
+div#results {
+  background-color: red;
+  padding: 40px 20px;
+  width: 100%;
+  color: #fff;
+}
+div#results p {
+  margin-bottom: 0px;
+}
+
+@media only screen and (min-width: 768px) {
+  div#results {
+    background-color: red;
+    padding: 40px 20px;
+    width: 50%;
+    margin: 0 auto;
+    color: #fff;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,20 +2,17 @@
   <div class="home">
     <!--<img alt="Vue logo" src="../assets/logo.png" />-->
     <Intro msg="Blood Pressure Calculator" />
-    <Results />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Intro from "@/components/Intro.vue";
-import Results from "@/components/Results.vue";
 
 export default {
   name: "home",
   components: {
-    Intro,
-    Results
+    Intro
   }
 };
 </script>
